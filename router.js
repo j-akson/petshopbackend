3d43b7407d8d0465454b54d750ee36e57373eb96
shebin5303284084 <shebin538@gamil.com>
2024-11-19 11:14:19 +0530

    shebin

--- router.js
+++ router.js
@@ -8,5 +8,5 @@ const petuser = require("./Modules/Petshop/petshopuserController");
 // Defining routes
 Router.post("/login", user.login);         
 Router.post("/petregister", petuser.petshopRegister); 
-
+Router.post("/petshopuserregistration",user.userRegistration)
 module.exports = Router;
